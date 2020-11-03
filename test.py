dbc39d3585f3c8e8ef79d34262e9cc0345e1d0c5
Arto Mulkahainen <arto.mulkahainen@gmail.com>
2020-11-03 10:14:02 +0200

    update

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import unittest
 from app import app
+import ast
 
 
 class RootTest(unittest.TestCase):
@@ -14,7 +15,6 @@ class RootTest(unittest.TestCase):
 
     def test_root(self):
         response = self.app.get("/api/posts", follow_redirects=True)
-        print(response.data)
         self.assertEqual(response.status_code, 200)
 
     def test_wrong_path_leads_to_404(self):
@@ -23,7 +23,18 @@ class RootTest(unittest.TestCase):
 
     def test_when_postcount_3_return_count_3(self):
         response = self.app.get("/api/posts", follow_redirects=True)
-        self.assertEqual(3, int(response.data["count"]))
+        mydata = ast.literal_eval(response.data.decode("UTF-8"))
+        for post in repr(mydata):
+            if post["id"] == 3:
+                print(post["post"])
+        self.assertEqual(3, int(repr(mydata["count"])))
+
+    """def test_id3_post_contains_jea(self):
+        response = self.app.get("/api/posts", follow_redirects=True)
+        mydata = ast.literal_eval(response.data.decode("UTF-8"))
+        for post in repr(mydata):
+            if int(post["id"]) == 3:
+                print(post["post"])"""
 
 
 if __name__ == "__main__":
--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,28 +1,40 @@
-import os
 import unittest
-
 from app import app
-from models.models import db
+import ast
 
 
-class BasicTest(unittest.TestCase):
-    # executed prior to each test
+class RootTest(unittest.TestCase):
     def setUp(self):
         app.config["TESTING"] = True
-        app.config["WTF_CSRF_ENABLED"] = False
         app.config["DEBUG"] = False
         self.app = app.test_client()
 
-        self.assertEqual(app.debug, False)
-
-    # executed after each test
     def tearDown(self):
+        # update soon
         pass
 
-    """def test_main_page(self):
+    def test_root(self):
         response = self.app.get("/api/posts", follow_redirects=True)
-        print(response)
-        self.assertEqual(response.status_code, 200)"""
+        self.assertEqual(response.status_code, 200)
+
+    def test_wrong_path_leads_to_404(self):
+        response = self.app.get("/api/posts/j", follow_redirects=True)
+        self.assertEqual(response.status_code, 404)
+
+    def test_when_postcount_3_return_count_3(self):
+        response = self.app.get("/api/posts", follow_redirects=True)
+        mydata = ast.literal_eval(response.data.decode("UTF-8"))
+        for post in repr(mydata):
+            if post["id"] == 3:
+                print(post["post"])
+        self.assertEqual(3, int(repr(mydata["count"])))
+
+    """def test_id3_post_contains_jea(self):
+        response = self.app.get("/api/posts", follow_redirects=True)
+        mydata = ast.literal_eval(response.data.decode("UTF-8"))
+        for post in repr(mydata):
+            if int(post["id"]) == 3:
+                print(post["post"])"""
 
 
 if __name__ == "__main__":
